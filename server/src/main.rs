8d05b534b638d25e2c745bc54cbf7a272c4c62c6
MasterDash5 <andrewbell25@icloud.com>
2024-10-22 11:48:18 -0600

    no way it works

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,16 +10,18 @@ struct Message {
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let input = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
+    let client: Client = Client::with_uri_str("mongodb://localhost").await?;
+    let messages: Collection<Message> = client.database("eteedir").collection("messages");
 
     if input.is_empty() {
         println!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA OH NO ERROR ERROR ERROR YOU HAVE ERROR.")
     } else {
-        println!("{}", input)
+        println!("{}", input);
+
+        messages
+            .insert_one(Message { content: input }, None)
+            .await?;
     };
-
-    let client = Client::with_uri_str("mongodb://localhost").await?;
-
-    let test: Collection<Message> = client.database("eteedir").collection("messages");
 
     Ok(())
 }
