177553dfe6836a2ba446e53494f0d432c0c98558
Josh Arbon <jarbon@student.neumont.edu>
2024-10-22 11:36:35 -0600

    check args on application startup

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,6 +11,12 @@ struct Message {
 async fn main() -> mongodb::error::Result<()> {
     let input = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
 
+    if input.is_empty() {
+        println!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA OH NO ERROR ERROR ERROR YOU HAVE ERROR.")
+    } else {
+        println!("{}", input)
+    };
+
     let client = Client::with_uri_str("mongodb://localhost").await?;
 
     let test: Collection<Message> = client.database("eteedir").collection("messages");
