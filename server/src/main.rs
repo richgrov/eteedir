8e275cce7d44f98d2c841ab94ed9d789163dcefb
timmyjinks <tysonjjenkins.github@gmail.com>
2024-10-22 11:09:27 -0600

    Connect to database

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mongodb::{bson::doc, Client, Collection};
+use serde::{Deserialize, Serialize};
+use tokio;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    content: String,
+}
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let client = Client::with_uri_str("mongodb://localhost").await?;
+
+    let test: Collection<Message> = client.database("eteedir").collection("messages");
+
+    Ok(())
 }
