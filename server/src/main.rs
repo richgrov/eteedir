3a72a97b52d083a303edf73d01da0e01ccfa3e08
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-10-22 11:26:13 -0600

    Read input from args

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,6 +9,8 @@ struct Message {
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
+    let input = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
+
     let client = Client::with_uri_str("mongodb://localhost").await?;
 
     let test: Collection<Message> = client.database("eteedir").collection("messages");
