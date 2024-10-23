fa72d0496e50f2012284037215a8159074b95b3e
Josh Arbon <jarbon@student.neumont.edu>
2024-10-23 11:20:29 -0600

    test this stuff richard

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("3 + 2 = {}", common::add(3, 2));
+    let options = ewebsock::Options::default();
+    // see documentation for more options
+    let (mut sender, receiver) = ewebsock::connect("ws://wss.postman-echo.com/raw", options).unwrap();
+    sender.send(ewebsock::WsMessage::Text("Hello!".into()));
+    while let Some(event) = receiver.try_recv() {
+        println!("Received {:?}", event);
+    }
+    println!("Shutting down...");
 }
