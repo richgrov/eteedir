1e51edbede5387b8ddcbeb3da51b0a1a3db009de
timmyjinks <tysonjjenkins.github@gmail.com>
2024-11-06 20:25:04 -0700

    Change the schema and refactor crud operations

--- server/src/cassandra.rs
+++ server/src/cassandra.rs
@@ -1,12 +1,11 @@
-use chrono::Utc;
 use futures::TryStreamExt;
 use scylla::transport::iterator::TypedRowIterator;
 use scylla::{IntoTypedRows, Session, SessionBuilder};
 use std::error::Error;
-use uuid::Uuid;
 
 pub struct Cassandra {
     pub session: Session,
+    pub last_id: i64,
 }
 
 impl Cassandra {
@@ -14,48 +13,48 @@ impl Cassandra {
         let uri = std::env::var("").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
         let session = SessionBuilder::new().known_node(uri).build().await?;
 
-        Ok(Cassandra { session })
-    }
-    pub async fn insert_content(&self, content: String) -> Result<(), Box<dyn Error>> {
-        let id = Uuid::new_v4();
+        let mut message = session
+            .query_unpaged("SELECT MAX(id) FROM eteedir.messages", &[])
+            .await?
+            .rows
+            .expect("Could not get max id")
+            .into_typed::<(i64,)>();
 
+        let id: i64 = message
+            .next()
+            .expect("error")
+            .expect("error strikes back")
+            .0;
+
+        Ok(Cassandra {
+            session,
+            last_id: id,
+        })
+    }
+
+    pub async fn insert_content(&mut self, content: String) -> Result<(), Box<dyn Error>> {
+        self.last_id += 1;
         self.session
             .query_unpaged(
                 "INSERT INTO eteedir.messages (id, message) VALUES(?, ?)",
-                (id, content),
+                (self.last_id, content),
             )
             .await?;
 
         Ok(())
     }
-    pub async fn update_content(
-        &self,
-        orginal: String,
-        update: String,
-    ) -> Result<(), Box<dyn Error>> {
-        let column = self
-            .session
-            .query_unpaged(
-                "SELECT id FROM eteedir.messages WHERE message = (?) ALLOW FILTERING",
-                (orginal,),
-            )
-            .await?
-            .rows;
 
-        if let Some(rows) = column {
-            for row in rows.into_typed::<(Uuid,)>() {
-                let uuid = row?.0;
-                self.session
-                    .query_unpaged(
-                        "UPDATE eteedir.messages SET message = (?) WHERE id = (?)",
-                        (update.clone(), uuid),
-                    )
-                    .await?;
-            }
-        }
+    pub async fn update_content(&self, id: i64, update: String) -> Result<(), Box<dyn Error>> {
+        self.session
+            .query_unpaged(
+                "UPDATE eteedir.messages SET message = (?) WHERE id = (?)",
+                (update.clone(), id),
+            )
+            .await?;
 
         Ok(())
     }
+
     pub async fn read_content(&self) -> Result<TypedRowIterator<(String,)>, Box<dyn Error>> {
         let mut messages = self
             .session
@@ -69,24 +68,12 @@ impl Cassandra {
 
         Ok(messages)
     }
-    pub async fn delete_content(&self, content: String) -> Result<(), Box<dyn Error>> {
-        let column = self
-            .session
-            .query_unpaged(
-                "SELECT id FROM eteedir.messages WHERE message = (?) ALLOW FILTERING",
-                (content,),
-            )
-            .await?
-            .rows;
 
-        if let Some(rows) = column {
-            for row in rows.into_typed::<(Uuid,)>() {
-                let uuid = row?.0;
-                self.session
-                    .query_unpaged("DELETE FROM eteedir.messages WHERE id = (?)", (uuid,))
-                    .await?;
-            }
-        }
+    pub async fn delete_content(&mut self, id: i64) -> Result<(), Box<dyn Error>> {
+        self.session
+            .query_unpaged("DELETE FROM eteedir.messages WHERE id = (?)", (id,))
+            .await?;
+        self.last_id = id;
 
         Ok(())
     }
--- server/src/main.rs
+++ server/src/main.rs
@@ -76,8 +76,8 @@ async fn main() {
         eprintln!(".env was not loaded");
     }
 
-    let server_address = std::env::var("address").expect("address not set");
-    let mongo_address = std::env::var("mongodb").expect("mongodb not set");
+    let server_address = std::env::var("ADDRESS").expect("ADDRESS not set");
+    let mongo_address = std::env::var("MONGODB").expect("MONGODB not set");
 
     let (inbound_msg_send, inbound_msg_recv) = mpsc::channel(64);
 
