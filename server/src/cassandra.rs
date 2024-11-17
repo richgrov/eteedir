caa499eef7d5427ef7bc1cd1bd0861e5d67129fe
timmyjinks <tysonjjenkins.github@gmail.com>
2024-11-16 17:20:12 -0700

    Add cassandra query requirements

--- server/src/cassandra.rs
+++ server/src/cassandra.rs
@@ -1,5 +1,7 @@
 use futures::TryStreamExt;
-use scylla::{IntoTypedRows, Session, SessionBuilder};
+use rand::Rng;
+use scylla::batch::Batch;
+use scylla::{Session, SessionBuilder};
 use serde::{Deserialize, Serialize};
 use std::error::Error;
 
@@ -10,7 +12,6 @@ pub struct Message {
 
 pub struct Cassandra {
     session: Session,
-    last_id: i64,
 }
 
 impl Cassandra {
@@ -18,42 +19,33 @@ impl Cassandra {
         let uri = address;
         let session = SessionBuilder::new().known_node(uri).build().await?;
 
-        let mut message = session
-            .query_unpaged("SELECT MAX(id) FROM eteedir.messages", &[])
-            .await?
-            .rows
-            .expect("Could not get max id")
-            .into_typed::<(i64,)>();
-
-        let id: i64 = message
-            .next()
-            .expect("error")
-            .expect("error strikes back")
-            .0;
-
-        Ok(Cassandra {
-            session,
-            last_id: id,
-        })
+        Ok(Cassandra { session })
     }
 
+    // IF
     pub async fn insert_message(&self, message: &Message) -> Result<(), Box<dyn Error>> {
-        // self.last_id += 1;
+        let id = rand::thread_rng().gen::<i64>();
         self.session
             .query_unpaged(
-                "INSERT INTO eteedir.messages (id, message) VALUES(?, ?)",
-                (self.last_id, message.content.clone()),
+                "INSERT INTO eteedir.messages (id, message, timestamp) VALUES(?, ?, ToTimeStamp(NOW())) IF NOT EXISTS",
+                (id, message.content.clone()),
             )
             .await?;
 
         Ok(())
     }
 
-    pub async fn update_message(&self, id: i64, update: String) -> Result<(), Box<dyn Error>> {
+    // TTL(Time to Live)
+    pub async fn insert_message_ttl(
+        &self,
+        message: &Message,
+        seconds: i32,
+    ) -> Result<(), Box<dyn Error>> {
+        let id = rand::thread_rng().gen::<i64>();
         self.session
             .query_unpaged(
-                "UPDATE eteedir.messages SET message = (?) WHERE id = (?)",
-                (update.clone(), id),
+                "INSERT INTO eteedir.messages (id, message, timestamp) VALUES(?, ?, ToTimeStamp(NOW())) USING TTL ?",
+                (id, message.content.clone(), seconds,),
             )
             .await?;
 
@@ -75,10 +67,106 @@ impl Cassandra {
         Ok(vec)
     }
 
+    // LIMIT
+    pub async fn read_n_messages(&self, limit: i32) -> Result<Vec<Message>, Box<dyn Error>> {
+        let mut messages = self
+            .session
+            .query_iter("SELECT message FROM eteedir.messages LIMIT ?", (limit,))
+            .await?
+            .into_typed::<(String,)>();
+
+        let mut vec = Vec::new();
+        while let Some((message,)) = messages.try_next().await? {
+            vec.push(Message { content: message });
+        }
+
+        Ok(vec)
+    }
+
+    // ORDER BY
+    pub async fn read_by_order(&self, id: i64) -> Result<Vec<Message>, Box<dyn Error>> {
+        let mut messages = self
+            .session
+            .query_iter(
+                "SELECT message FROM eteedir.messages WHERE id = (?) ORDER BY timestamp",
+                (id,),
+            )
+            .await?
+            .into_typed::<(String,)>();
+
+        let mut vec = Vec::new();
+        while let Some((message,)) = messages.try_next().await? {
+            vec.push(Message { content: message });
+        }
+
+        Ok(vec)
+    }
+
+    // IN
+    pub async fn read_by_in(&self, id1: i64, id2: i64) -> Result<Vec<Message>, Box<dyn Error>> {
+        let mut messages = self
+            .session
+            .query_iter(
+                "SELECT message FROM eteedir.messages WHERE id in (?, ?)",
+                (id1, id2),
+            )
+            .await?
+            .into_typed::<(String,)>();
+
+        let mut vec = Vec::new();
+        while let Some((message,)) = messages.try_next().await? {
+            vec.push(Message { content: message });
+        }
+
+        Ok(vec)
+    }
+
+    pub async fn update_message(
+        &self,
+        id: i64,
+        timestamp: String,
+        update: String,
+    ) -> Result<(), Box<dyn Error>> {
+        let datetime = chrono::DateTime::parse_from_str(&timestamp, "%Y-%m-%d %H:%M:%S%.f%z")?
+            .with_timezone(&chrono::Utc)
+            .timestamp_millis();
+        let timestamp = scylla::frame::value::CqlTimestamp(datetime);
+        self.session
+            .query_unpaged(
+                "UPDATE eteedir.messages SET message = (?) WHERE id = (?) AND timestamp = (?)",
+                (update.clone(), id, timestamp),
+            )
+            .await?;
+
+        Ok(())
+    }
+
     pub async fn delete_message(&mut self, id: i64) -> Result<(), Box<dyn Error>> {
         self.session
             .query_unpaged("DELETE FROM eteedir.messages WHERE id = (?)", (id,))
             .await?;
+
+        Ok(())
+    }
+
+    // BATCH
+    pub async fn replace_user(
+        &self,
+        username: String,
+        old_user_id: i64,
+    ) -> Result<(), Box<dyn Error>> {
+        let id = rand::thread_rng().gen::<i64>();
+        let mut batch: Batch = Default::default();
+
+        batch.append_statement("DELETE FROM eteedir.user WHERE id = (?) ");
+        batch.append_statement("INSERT INTO eteedir.user (id, username) VALUES (?, ?)");
+
+        let batch_values = ((old_user_id,), (id, username));
+
+        self.session
+            .batch(&batch, batch_values)
+            .await
+            .expect("Batch failed to batch things");
 
         Ok(())
     }
